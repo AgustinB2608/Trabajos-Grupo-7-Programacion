a0e19aec52a38f60dc708cd11ce9e41bec1f1a8c
Abril <lopez.abrilagustina6@gmail.com>
2024-09-10 16:46:15 -0300

    Creacion tp4, ddl provincias vinculada con base de datos viajes

---
+++ Trabajo Practico 4/TP4_Grupo_7/Ejercicio1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio1.aspx.cs" Inherits="TP4_Grupo_7.Ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Destino inicial:<br />
+            <br />
+            Provincia:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="ddlProvincias" runat="server">
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Trabajo Practico 4/TP4_Grupo_7/Ejercicio1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+
+namespace TP4_Grupo_7
+{
+    public partial class Ejercicio1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                SqlConnection cn = new SqlConnection("Data Source=localhost\\sqlexpress;Initial Catalog=Viajes;Integrated Security=True");
+
+                cn.Open();
+
+                SqlDataAdapter adapt = new SqlDataAdapter("Select * From Provincias", cn); 
+
+                DataSet ds = new DataSet();
+                adapt.Fill(ds, "TablaProvincias");
+
+                ddlProvincias.DataSource = ds.Tables[0]; // 0 = TablaProvincias
+                ddlProvincias.DataTextField = "NombreProvincia";
+                ddlProvincias.DataValueField = "idProvincia";
+
+                ddlProvincias.DataBind();
+
+                cn.Close();
+            }   
+        }
+    }
+}
