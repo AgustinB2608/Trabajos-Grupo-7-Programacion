be0053a2d6a46ab2c0b7db0d6a0c16109bb162af
juanma01234 <juanmaasselborn@gmail.com>
2024-09-12 03:56:43 -0300

    Realizo la conexion a la base de datos neptuno, hago las consultas para traer la informacion y brindo la misma al ddlproducto.

--- Trabajo Practico 4/TP4_Grupo_7/Ejercicio2.aspx.cs
+++ Trabajo Practico 4/TP4_Grupo_7/Ejercicio2.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,29 @@ namespace TP4_Grupo_7
 {
     public partial class Ejercicio2 : System.Web.UI.Page
     {
+        private String rutaNeptunoSQL = "Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security=True";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack == false)
+            {
+                CargarProducto();
+            }
+        }
+           private void CargarProducto()
+            {
+                SqlConnection cn = new SqlConnection(rutaNeptunoSQL);
+                cn.Open();
+
+                SqlDataAdapter adapt = new SqlDataAdapter("Select IdProducto, NombreProducto From ListaDeProductosActual", cn);
+                DataSet ds = new DataSet();
+                adapt.Fill(ds, "Productos");
+
+            ddlProducto.DataSource = ds.Tables[0];
+            ddlProducto.DataTextField = "NombreProducto";
+            ddlProducto.DataValueField = "IdProducto";
+            ddlProducto.DataBind();
+            ddlProducto.Items.Insert(0, new ListItem("--Seleccionar--")); 
 
         }
+        }
     }
-}
