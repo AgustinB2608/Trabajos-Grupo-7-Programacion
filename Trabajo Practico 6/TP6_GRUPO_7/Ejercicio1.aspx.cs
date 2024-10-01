06e6856dd1fcf94c17b5adedac907c6e98c6dbec
AgustinB2608 <agustin.bernal@alumnos.frgp.utn.edu.ar>
2024-10-01 18:44:44 -0300

    Aporte

--- Trabajo Practico 6/TP6_GRUPO_7/Ejercicio1.aspx.cs
+++ Trabajo Practico 6/TP6_GRUPO_7/Ejercicio1.aspx.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
 using System.Data;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.EnterpriseServices;
 
 namespace TP6_GRUPO_7
 {
