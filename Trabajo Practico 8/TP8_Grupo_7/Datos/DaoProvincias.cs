15e73c1f72dfc59746ddcc9d89b71712d09d4358
maxiifabeiro <machilux_08@hotmail.com>
2024-10-17 15:03:02 -0300

    DaoProvincias y Proyecto NEGOCIO

---
+++ Trabajo Practico 8/TP8_Grupo_7/Datos/DaoProvincias.cs
@@ -0,0 +1,41 @@
+﻿using ENTIDADES;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DaoProvincias
+    {
+        private Conexion ds = new Conexion();
+
+        // Metodo para obtener lista de todas las provincias desde la base de datos.
+        public List<Provincias> ObtenerProvincias()
+        {
+            string consulta = "SELECT Id_Provincia, DescripcionProvincia FROM Provincia";
+
+            DataTable dt = ds.EjecutarConsulta(consulta);
+
+            // Creo una lista de objetos Provincia para almacenar los resultados.
+            List<Provincias> provincias = new List<Provincias>();
+
+            // Recorre cada fila del DataTable.
+            foreach (DataRow row in dt.Rows)
+            {
+                // Creo un nuevo objeto Provincia y asigna los valores de la fila actual.
+                Provincias provincia = new Provincias
+                {
+                    Id_Provincia = (int)row["Id_Provincia"], 
+                    DescripcionProvincia1 = row["DescripcionProvincia"].ToString().ToCharArray() 
+                };
+
+                // Agrega la provincia a la lista.
+                provincias.Add(provincia);
+            }
+            return provincias;
+        }
+    }
+}
---
+++ Trabajo Practico 8/TP8_Grupo_7/NEGOCIO/NegocioProvincia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using ENTIDADES;
+namespace NEGOCIO
+{
+    public class NegocioProvincia
+    {
+        private DaoProvincias ltProvincias = new DaoProvincias();
+
+        // Obtengo la lista de provincias llamando al metodo ObtenerProvincias de DaoProvincias
+        public List<Provincias> ObtenerProvincias()
+        {
+            return ltProvincias.ObtenerProvincias();
+        }
+    }
+}
