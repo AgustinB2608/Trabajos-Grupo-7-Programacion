5b675200f8da1f3b7db289ad235c8ce17c03459a
maxiifabeiro <machilux_08@hotmail.com>
2024-12-05 11:28:05 -0300

    btn redirect a eliminarUsuario

--- TPINT_GRUPO_7_1/VISTAS/EliminarMedico.aspx.designer.cs
+++ TPINT_GRUPO_7_1/VISTAS/EliminarMedico.aspx.designer.cs
@@ -51,6 +51,15 @@ namespace VISTAS
         protected global::System.Web.UI.WebControls.Button btnEliminar;
 
         /// <summary>
+        /// Control btnEliminarUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminarUsuario;
+
+        /// <summary>
         /// Control hlkEliminar.
         /// </summary>
         /// <remarks>
