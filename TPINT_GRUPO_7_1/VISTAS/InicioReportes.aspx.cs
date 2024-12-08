966abf45e5c4ca86acd8d7e7314cf9859b3b1734
Abril <lopez.abrilagustina6@gmail.com>
2024-12-08 16:50:32 -0300

    Sesion en menú reportes

--- TPINT_GRUPO_7_1/VISTAS/InicioReportes.aspx.cs
+++ TPINT_GRUPO_7_1/VISTAS/InicioReportes.aspx.cs
@@ -9,8 +9,21 @@ namespace VISTAS
 {
     public partial class RealizarInformes : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["UsuarioLegajo"] != null && Session["UsuarioTipo"] != null && Session["UsuarioTipo"].ToString() == "A")
+            {
+                string nombre = Session["UsuarioNombre"].ToString(); // Nombre
+                string apellido = Session["UsuarioApellido"].ToString(); // Apellido
+                string tipoUsuario = Session["UsuarioTipo"].ToString(); // Tipo de usuario
+
+                lblUsuario.Text = $"{nombre} {apellido} {tipoUsuario}";
+            }
+            else
+            {
+                Response.Redirect("InicioLogin.aspx"); // Redirigir si no es un administrador logueado
+            }
 
         }
 
