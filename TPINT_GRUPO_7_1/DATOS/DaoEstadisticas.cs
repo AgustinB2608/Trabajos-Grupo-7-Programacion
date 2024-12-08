d51cbead717e3c0b099050527bc52911f8133d9a
OrianaManrique <orianamanriqueutlook.com.ar>
2024-12-07 22:09:08 -0300

    reportes

--- TPINT_GRUPO_7_1/DATOS/DaoEstadisticas.cs
+++ TPINT_GRUPO_7_1/DATOS/DaoEstadisticas.cs
@@ -27,5 +27,93 @@ namespace DATOS
             return ds.EjecutarProcedimientoConParametro(procedimiento, parametros);
         }
 
+        public DataTable ObtenerPacientesMes(int mes, string estado)
+        {
+            string consulta = "EXEC SP_PacientesPresentesoAusentes @mes, @estado";
+
+            SqlParameter[] parametros = new SqlParameter[]
+            {
+                new SqlParameter("@mes", mes),
+                new SqlParameter("@estado", estado)
+            };
+
+            return ds.EjecutarProcedimientoConParametro(consulta, parametros);
+        }
+
+        public DataTable TotalTurnos()//total turnos tabla
+        {
+            string consulta = "SELECT COUNT(CodTurno_TU) AS 'Turnos Totales' FROM Turnos ";
+
+            return ds.EjecutarConsulta(consulta);
+        }
+
+        public DataTable TotalTurnosMes(int mes)//total turnos de determinado mes
+        {
+            int mesActual = DateTime.Now.Month;
+            if (mes < mesActual)
+            {
+
+                string consulta = "SELECT COUNT(CodTurno_TU) AS 'Turnos Totales' FROM Turnos " +
+                "WHERE EstadoEtapa_TU = 'A' OR EstadoEtapa_TU = 'P' AND MONTH(Dia_TU) = @mes";
+
+                SqlParameter[] parametros = new SqlParameter[]
+                {
+                new SqlParameter("@mes", mes)
+                };
+
+                return ds.EjecutarConsultaConParametros(consulta, parametros);
+
+            }
+            else if (mes == mesActual)//total turnos del mes actual, contempla los no atendidos tambien
+            {
+                string consulta = "SELECT COUNT(CodTurno_TU) AS 'Turnos Totales' FROM Turnos " +
+                "WHERE EstadoEtapa_TU = 'A' OR EstadoEtapa_TU = 'P' OR EstadoEtapa_TU = 'N' AND MONTH(Dia_TU) = @mes";
+
+                SqlParameter[] parametros = new SqlParameter[]
+                {
+                new SqlParameter("@mes", mes)
+                };
+
+                return ds.EjecutarConsultaConParametros(consulta, parametros);
+            }
+
+            else return null;
+        }
+
+        public DataTable TotalTurnosSegunEstadoyMes(int mes, string estado)
+        {
+            string consulta = "SELECT EstadoEtapa_TU AS '(A)usente o (P)resente', COUNT(CodTurno_TU) AS Total FROM Turnos "+
+            "WHERE EstadoEtapa_TU = @estado AND MONTH(Dia_TU) = @mes GROUP BY EstadoEtapa_TU; ";
+
+            SqlParameter[] parametros = new SqlParameter[]
+            {
+                new SqlParameter("@mes", mes),
+                new SqlParameter("@estado", estado)
+            };
+
+            return ds.EjecutarProcedimientoConParametro(consulta, parametros);
+        }
+
+        public DataTable TotalTurnosPorEstadoGlobal()
+        {
+            string consulta = "SELECT EstadoEtapa_TU AS Estado, COUNT(EstadoEtapa_TU) AS Total FROM Turnos "+
+            "GROUP BY EstadoEtapa_TU; ";
+
+            return ds.EjecutarConsulta(consulta);
+        }
+        public DataTable TotalTurnosSegunEstadoGlobal(string estado)
+        {
+            string consulta = "SELECT EstadoEtapa_TU AS '(A)usentes o (P)resentes', COUNT(CodTurno_TU) AS Total "+
+            "FROM Turnos WHERE EstadoEtapa_TU = @estado GROUP BY EstadoEtapa_TU; ";
+
+            SqlParameter[] parametros = new SqlParameter[]
+           {
+                new SqlParameter("@estado", estado)
+           };
+
+            return ds.EjecutarProcedimientoConParametro(consulta, parametros);
+
+        }
+
     }
 }
--- TPINT_GRUPO_7_1/NEGOCIOS/NegocioEstadisticas.cs
+++ TPINT_GRUPO_7_1/NEGOCIOS/NegocioEstadisticas.cs
@@ -13,10 +13,42 @@ using DATOS;
 {
    public class NegocioEstadisticas
     {
+        DaoEstadisticas dao = new DaoEstadisticas();
         public DataTable ObtenerEstadisticasMensuales(string mes, string codEspecialidad)
         {
-            DaoEstadisticas dao = new DaoEstadisticas();
             return dao.ObtenerEstadisticasMensuales(mes, codEspecialidad);
         }
+
+        public DataTable ObtenerPacientesMes(int mes, string estado)
+        {
+            return dao.ObtenerPacientesMes(mes, estado);
+        }
+
+        public DataTable TotalTurnos()
+        {
+            return dao.TotalTurnos();
+        }
+
+        public DataTable TotalTurnosMes(int mes)
+        {
+            return dao.TotalTurnosMes(mes);
+        }
+
+        public DataTable TotalTurnosSegunEstadoyMes(int mes, string estado)
+        {
+            return dao.TotalTurnosSegunEstadoyMes(mes, estado);
+        }
+
+        public DataTable TotalTurnosPorEstadoGlobal()
+        {
+            return dao.TotalTurnosPorEstadoGlobal();
+        }
+
+        public DataTable TotalTurnosSegunEstadoGlobal(string estado)
+        {
+            return dao.TotalTurnosSegunEstadoGlobal(estado);
+        }
+
+
     }
 }
